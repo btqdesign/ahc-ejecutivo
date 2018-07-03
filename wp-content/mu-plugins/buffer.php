dd8d9d44ced4f50f99189a31e7de1250c02dbe44
idevol <saul.diaz.v@gmail.com>
2018-07-02 20:03:28 -0500

    HTTP a HTTPS

--- wp-content/mu-plugins/buffer.php
+++ wp-content/mu-plugins/buffer.php
@@ -29,10 +29,36 @@ add_action(
 );
 
 add_filter('final_output', function($output) {
-	// Soporte HTTPS
-	$output = str_replace('http:', 'https:', $output);
-	$output = str_replace('https://schemas.xmlsoap.org', 'http://schemas.xmlsoap.org', $output);
-	$output = str_replace('https://docs.oasisopen.org', 'http://docs.oasisopen.org', $output);
-	$output = str_replace('https://www.sitemaps.org', 'http://www.sitemaps.org', $output);
+    $patt = array(
+        //'/((src|href|action|srcset|xmlns)=("|\'))(\s*)(https?)(:)(\/\/)/'
+        //,'/(url\(("|\'))(https?)(:)(\/\/)/'
+        //,'/(&quot;)(https?)(:)(\\\\\/\\\\\/)/'
+        //,'/("|\')(https?)(:)(\\\\\/\\\\\/)/'
+        //,'/(,\s*("|\')?)(https?)(:)(\/\/)/'
+        '/(("|\')https?:\/\/((ejecutivo\.idevol\.net)|(hotelelejecutivo\.com\.mx))\/[a-zA-Z0-9\._\/-]*\.(css|js))(\?(ver|version)=[a-zA-Z0-9%_.-]{1,8})("|\')/'
+    );
+    $repl = array(
+        //'${1}${7}'
+        //,'${1}${5}'
+        //,'${1}${4}'
+        //,'${1}${4}'
+        //,'${1}${5}'
+        '${1}${9}'
+    );
+    $output = preg_replace($patt, $repl, $output);
+    
+    // Soporte HTTPS
+    $output = str_replace('http:', 'https:', $output);
+    $output = str_replace('https://schemas.xmlsoap.org', 'http://schemas.xmlsoap.org', $output);
+    $output = str_replace('https://docs.oasisopen.org',  'http://docs.oasisopen.org',  $output);
+    $output = str_replace('https://www.sitemaps.org',    'http://www.sitemaps.org',    $output);
+    
+    return $output;
+});
+
+add_filter('wp_redirect', function($output) {
+	$output = str_replace('http://ejecutivo.idevol.net',    'https://ejecutivo.idevol.net',    $output);
+	$output = str_replace('http://hotelelejecutivo.com.mx', 'https://hotelelejecutivo.com.mx', $output);
+	  
 	return $output;
 });
